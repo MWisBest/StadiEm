33847fedb0f495d9aec8cdeb9ab52ced51059e9a
Kyle Repinski <repinski23@gmail.com>
2020-11-22 10:57:51 -0600

    StadiaController: Finally support remapping.

--- StadiEm/Device/Stadia/StadiaController.cs
+++ StadiEm/Device/Stadia/StadiaController.cs
@@ -27,8 +27,7 @@ namespace StadiEm.Device.Stadia
 		public const int DATA_L2 = 0x08;
 		public const int DATA_R2 = 0x09;
 
-		public Dictionary<Xbox360Property, bool> dictttt;
-
+		public Dictionary<Type, Xbox360Property> xboxMap;
 
 		public Thread ssThread, vidThread, inputThread, writeThread;
 		private AutoResetEvent writeEvent;
@@ -36,6 +35,31 @@ namespace StadiEm.Device.Stadia
 
 		public StadiaController( HidDevice device, HidStream stream, ViGEmClient client, int index ) : base( device, stream, client, index )
 		{
+			xboxMap = new Dictionary<Type, Xbox360Property>
+			{
+				[typeof( StadiaButton.A )] = Xbox360Button.A,
+				[typeof( StadiaButton.B )] = Xbox360Button.B,
+				[typeof( StadiaButton.X )] = Xbox360Button.X,
+				[typeof( StadiaButton.Y )] = Xbox360Button.Y,
+				[typeof( StadiaButton.Up )] = Xbox360Button.Up,
+				[typeof( StadiaButton.Down )] = Xbox360Button.Down,
+				[typeof( StadiaButton.Left )] = Xbox360Button.Left,
+				[typeof( StadiaButton.Right )] = Xbox360Button.Right,
+				[typeof( StadiaButton.L1 )] = Xbox360Button.LeftShoulder,
+				[typeof( StadiaButton.R1 )] = Xbox360Button.RightShoulder,
+				[typeof( StadiaButton.L3 )] = Xbox360Button.LeftThumb,
+				[typeof( StadiaButton.R3 )] = Xbox360Button.RightThumb,
+				[typeof( StadiaButton.Select )] = Xbox360Button.Back,
+				[typeof( StadiaButton.Start )] = Xbox360Button.Start,
+				[typeof( StadiaButton.Stadia )] = Xbox360Button.Guide,
+				[typeof( StadiaAxis.LX )] = Xbox360Axis.LeftThumbX,
+				[typeof( StadiaAxis.LY )] = Xbox360Axis.LeftThumbY,
+				[typeof( StadiaAxis.RX )] = Xbox360Axis.RightThumbX,
+				[typeof( StadiaAxis.RY )] = Xbox360Axis.RightThumbY,
+				[typeof( StadiaSlider.L2 )] = Xbox360Slider.LeftTrigger,
+				[typeof( StadiaSlider.R2 )] = Xbox360Slider.RightTrigger,
+			};
+
 			target360.FeedbackReceived += this.Target360_FeedbackReceived;
 			targetDS4.FeedbackReceived += this.TargetDS4_FeedbackReceived;
 
@@ -184,7 +208,8 @@ WRITE_STREAM_FAILURE:
 		{
 			bool ss_button_held = false;
 			bool assistant_button_held = false;
-			bool instant_trigger_release = true;
+			bool instant_trigger_release = false;
+			bool round_left_stick_corners = false;
 			bool instantReleaseL = false;
 			bool instantReleaseR = false;
 			_stream.ReadTimeout = Timeout.Infinite;
@@ -270,6 +295,28 @@ WRITE_STREAM_FAILURE:
 						}
 					}*/
 
+					byte curLX = report.LX;
+					byte curLY = report.LY;
+					if( round_left_stick_corners )
+					{
+						if( curLX < 0x2A )
+						{
+							curLX >>= 1;
+						}
+						else if( curLX > 0xD5 )
+						{
+							curLX = (byte)( curLX + ( ( ( 0xFF - curLX ) >> 1 ) & 0xFF ) );
+						}
+						if( curLY < 0x2A )
+						{
+							curLY >>= 1;
+						}
+						else if( curLY > 0xD5 )
+						{
+							curLY = (byte)( curLY + ( ( ( 0xFF - curLY ) >> 1 ) & 0xFF ) );
+						}
+					}
+
 					byte curL2 = report.L2;
 					byte curR2 = report.R2;
 					if( instant_trigger_release )
@@ -309,29 +356,65 @@ WRITE_STREAM_FAILURE:
 						}
 					}
 
-					target360.SetButtonState( Xbox360Button.A, report.A );
-					target360.SetButtonState( Xbox360Button.B, report.B );
-					target360.SetButtonState( Xbox360Button.X, report.X );
-					target360.SetButtonState( Xbox360Button.Y, report.Y );
-					target360.SetButtonState( Xbox360Button.Up, report.Up );
-					target360.SetButtonState( Xbox360Button.Down, report.Down );
-					target360.SetButtonState( Xbox360Button.Left, report.Left );
-					target360.SetButtonState( Xbox360Button.Right, report.Right );
-					target360.SetButtonState( Xbox360Button.LeftShoulder, report.L1 );
-					target360.SetButtonState( Xbox360Button.RightShoulder, report.R1 );
-					target360.SetButtonState( Xbox360Button.LeftThumb, report.L3 );
-					target360.SetButtonState( Xbox360Button.RightThumb, report.R3 );
-					target360.SetButtonState( Xbox360Button.Back, report.Select );
-					target360.SetButtonState( Xbox360Button.Start, report.Start );
-					target360.SetButtonState( Xbox360Button.Guide, report.Stadia );
+					// Save current report before modifying it with additional features
+					report.CopyValuesTo( report_prev );
+					// Modify current report values before translating to Xbox
+					report.L2.Value = curL2;
+					report.R2.Value = curR2;
+					report.LX.Value = curLX;
+					report.LY.Value = curLY;
 
-					target360.SetAxisValue( Xbox360Axis.LeftThumbX, report.LX );
-					target360.SetAxisValue( Xbox360Axis.LeftThumbY, report.LY );
-					target360.SetAxisValue( Xbox360Axis.RightThumbX, report.RX );
-					target360.SetAxisValue( Xbox360Axis.RightThumbY, report.RY );
+					// reset report in case profile updates as we're running
+					target360.ResetReport();
+					foreach( StadiaProperty prop in report.Props )
+					{
+						Type stadiaType = prop.GetType();
+						if( xboxMap.TryGetValue( stadiaType, out Xbox360Property xboxProp ) )
+						{
+							if( xboxProp is Xbox360Button xbutton )
+							{
+								if( prop is StadiaButton sbutton )
+								{
+									target360.SetButtonState( xbutton, sbutton );
+								}
+								else if( prop is StadiaSlider sslider )
+								{
+									target360.SetButtonState( xbutton, sslider );
+								}
+								else if( prop is StadiaAxis saxis )
+								{
+									target360.SetButtonState( xbutton, saxis );
+								}
+							}
+							else if( xboxProp is Xbox360Slider xslider )
+							{
+								if( prop is StadiaSlider sslider )
+								{
+									target360.SetSliderValue( xslider, sslider );
+								}
+								else if( prop is StadiaButton sbutton )
+								{
+									target360.SetSliderValue( xslider, sbutton );
+								}
+								else if( prop is StadiaAxis saxis )
+								{
+									target360.SetSliderValue( xslider, saxis );
+								}
+							}
+							else if( xboxProp is Xbox360Axis xaxis )
+							{
+								if( prop is StadiaAxis saxis )
+								{
+									target360.SetAxisValue( xaxis, saxis );
+								}
+								else
+								{
+									throw new NotImplementedException();
+								}
+							}
+						}
+					}
 
-					target360.SetSliderValue( Xbox360Slider.LeftTrigger, curL2 );
-					target360.SetSliderValue( Xbox360Slider.RightTrigger, curR2 );
 					target360.SubmitReport();
 
 					if( report.Screenshot && !ss_button_held )
@@ -369,8 +452,6 @@ WRITE_STREAM_FAILURE:
 					{
 						assistant_button_held = false;
 					}
-
-					report.CopyValuesTo( report_prev );
 				}
 			}
 			return;
@@ -381,32 +462,62 @@ INPUT_STREAM_FAILURE:
 
 		public class StadiaReport
 		{
-			public StadiaButton A = new StadiaButton( "A" );
-			public StadiaButton B = new StadiaButton( "B" );
-			public StadiaButton X = new StadiaButton( "X" );
-			public StadiaButton Y = new StadiaButton( "Y" );
-			public StadiaButton Up = new StadiaButton( "Up" );
-			public StadiaButton Down = new StadiaButton( "Down" );
-			public StadiaButton Left = new StadiaButton( "Left" );
-			public StadiaButton Right = new StadiaButton( "Right" );
-			public StadiaButton L1 = new StadiaButton( "L1" );
-			public StadiaButton R1 = new StadiaButton( "R1" );
-			public StadiaButton L3 = new StadiaButton( "L3" );
-			public StadiaButton R3 = new StadiaButton( "R3" );
-			public StadiaButton Assistant = new StadiaButton( "Assistant" );
-			public StadiaButton Screenshot = new StadiaButton( "Screenshot" );
-			public StadiaButton Select = new StadiaButton( "Select" );
-			public StadiaButton Start = new StadiaButton( "Start" );
-			public StadiaButton Stadia = new StadiaButton( "Stadia" );
-			public StadiaAxis LX = new StadiaAxis( "LX", true );
-			public StadiaAxis LY = new StadiaAxis( "LY", false );
-			public StadiaAxis RX = new StadiaAxis( "RX", true );
-			public StadiaAxis RY = new StadiaAxis( "RY", false );
-			public StadiaSlider L2 = new StadiaSlider( "L2" );
-			public StadiaSlider R2 = new StadiaSlider( "R2" );
+			public StadiaProperty[] Props
+			{
+				get;
+			}
+			public StadiaButton.A A = new StadiaButton.A();
+			public StadiaButton.B B = new StadiaButton.B();
+			public StadiaButton.X X = new StadiaButton.X();
+			public StadiaButton.Y Y = new StadiaButton.Y();
+			public StadiaButton.Up Up = new StadiaButton.Up();
+			public StadiaButton.Down Down = new StadiaButton.Down();
+			public StadiaButton.Left Left = new StadiaButton.Left();
+			public StadiaButton.Right Right = new StadiaButton.Right();
+			public StadiaButton.L1 L1 = new StadiaButton.L1();
+			public StadiaButton.R1 R1 = new StadiaButton.R1();
+			public StadiaButton.L3 L3 = new StadiaButton.L3();
+			public StadiaButton.R3 R3 = new StadiaButton.R3();
+			public StadiaButton.Assistant Assistant = new StadiaButton.Assistant();
+			public StadiaButton.Screenshot Screenshot = new StadiaButton.Screenshot();
+			public StadiaButton.Select Select = new StadiaButton.Select();
+			public StadiaButton.Start Start = new StadiaButton.Start();
+			public StadiaButton.Stadia Stadia = new StadiaButton.Stadia();
+			public StadiaAxis.LX LX = new StadiaAxis.LX();
+			public StadiaAxis.LY LY = new StadiaAxis.LY();
+			public StadiaAxis.RX RX = new StadiaAxis.RX();
+			public StadiaAxis.RY RY = new StadiaAxis.RY();
+			public StadiaSlider.L2 L2 = new StadiaSlider.L2();
+			public StadiaSlider.R2 R2 = new StadiaSlider.R2();
 
 			public StadiaReport()
 			{
+				Props = new StadiaProperty[]
+				{
+					A,
+					B,
+					X,
+					Y,
+					Up,
+					Down,
+					Left,
+					Right,
+					L1,
+					R1,
+					L3,
+					R3,
+					Assistant,
+					Screenshot,
+					Select,
+					Start,
+					Stadia,
+					LX,
+					LY,
+					RX,
+					RY,
+					L2,
+					R2
+				};
 			}
 
 			public bool PopulateFromReport( byte[] report )
@@ -480,25 +591,25 @@ INPUT_STREAM_FAILURE:
 
 			public void CopyValuesTo( StadiaReport other )
 			{
-				other.A.Value = A.Value;
-				other.B.Value = B.Value;
-				other.X.Value = X.Value;
-				other.Y.Value = Y.Value;
-				other.L1.Value = L1.Value;
-				other.R1.Value = R1.Value;
-				other.L3.Value = L3.Value;
-				other.R3.Value = R3.Value;
-				other.Screenshot.Value = Screenshot.Value;
-				other.Assistant.Value = Assistant.Value;
-				other.Start.Value = Start.Value;
-				other.Select.Value = Select.Value;
-				other.Stadia.Value = Stadia.Value;
-				other.LX.Value = LX.Value;
-				other.LY.Value = LY.Value;
-				other.RX.Value = RX.Value;
-				other.RY.Value = RY.Value;
-				other.L2.Value = L2.Value;
-				other.R2.Value = R2.Value;
+				other.A.Value = A;
+				other.B.Value = B;
+				other.X.Value = X;
+				other.Y.Value = Y;
+				other.L1.Value = L1;
+				other.R1.Value = R1;
+				other.L3.Value = L3;
+				other.R3.Value = R3;
+				other.Screenshot.Value = Screenshot;
+				other.Assistant.Value = Assistant;
+				other.Start.Value = Start;
+				other.Select.Value = Select;
+				other.Stadia.Value = Stadia;
+				other.LX.Value = LX;
+				other.LY.Value = LY;
+				other.RX.Value = RX;
+				other.RY.Value = RY;
+				other.L2.Value = L2;
+				other.R2.Value = R2;
 			}
 		}
 
@@ -524,7 +635,112 @@ INPUT_STREAM_FAILURE:
 			{
 			}
 
+			public class A : StadiaButton
+			{
+				public A() : base( "A" )
+				{
+				}
+			}
+			public class B : StadiaButton
+			{
+				public B() : base( "B" )
+				{
+				}
+			}
+			public class X : StadiaButton
+			{
+				public X() : base( "X" )
+				{
+				}
+			}
+			public class Y : StadiaButton
+			{
+				public Y() : base( "Y" )
+				{
+				}
+			}
+			public class Up : StadiaButton
+			{
+				public Up() : base( "Up" )
+				{
+				}
+			}
+			public class Down : StadiaButton
+			{
+				public Down() : base( "Down" )
+				{
+				}
+			}
+			public class Left : StadiaButton
+			{
+				public Left() : base( "Left" )
+				{
+				}
+			}
+			public class Right : StadiaButton
+			{
+				public Right() : base( "Right" )
+				{
+				}
+			}
+			public class L1 : StadiaButton
+			{
+				public L1() : base( "L1" )
+				{
+				}
+			}
+			public class R1 : StadiaButton
+			{
+				public R1() : base( "R1" )
+				{
+				}
+			}
+			public class L3 : StadiaButton
+			{
+				public L3() : base( "L3" )
+				{
+				}
+			}
+			public class R3 : StadiaButton
+			{
+				public R3() : base( "R3" )
+				{
+				}
+			}
+			public class Assistant : StadiaButton
+			{
+				public Assistant() : base( "Assistant" )
+				{
+				}
+			}
+			public class Screenshot : StadiaButton
+			{
+				public Screenshot() : base( "Screenshot" )
+				{
+				}
+			}
+			public class Select : StadiaButton
+			{
+				public Select() : base( "Select" )
+				{
+				}
+			}
+			public class Start : StadiaButton
+			{
+				public Start() : base( "Start" )
+				{
+				}
+			}
+			public class Stadia : StadiaButton
+			{
+				public Stadia() : base( "Stadia" )
+				{
+				}
+			}
+
 			public static implicit operator bool( StadiaButton b ) => b.Value;
+
+			public static implicit operator byte( StadiaButton b ) => b.Value ? byte.MaxValue : byte.MinValue;
 		}
 
 		public class StadiaAxis : StadiaProperty
@@ -545,7 +761,35 @@ INPUT_STREAM_FAILURE:
 				this.IsXaxis = isxaxis;
 			}
 
+			public class LX : StadiaAxis
+			{
+				public LX() : base( "LX", true )
+				{
+				}
+			}
+			public class LY : StadiaAxis
+			{
+				public LY() : base( "LY", false )
+				{
+				}
+			}
+			public class RX : StadiaAxis
+			{
+				public RX() : base( "RX", true )
+				{
+				}
+			}
+			public class RY : StadiaAxis
+			{
+				public RY() : base( "RY", false )
+				{
+				}
+			}
+
 			public static implicit operator byte( StadiaAxis a ) => a.Value;
+
+			// TODO: Configure this.
+			public static implicit operator bool( StadiaAxis a ) => ( Math.Abs(a.Value - 0x80) > 0x40 );
 
 			public static implicit operator short( StadiaAxis a )
 			{
@@ -595,7 +839,23 @@ INPUT_STREAM_FAILURE:
 			{
 			}
 
+			public class L2 : StadiaSlider
+			{
+				public L2() : base( "L2" )
+				{
+				}
+			}
+			public class R2 : StadiaSlider
+			{
+				public R2() : base( "R2" )
+				{
+				}
+			}
+
 			public static implicit operator byte( StadiaSlider s ) => s.Value;
+
+			// TODO: Add different modes for slider to bool/button conversion
+			public static implicit operator bool( StadiaSlider s ) => s.Value >= 0x40;
 		}
 	}
 }
