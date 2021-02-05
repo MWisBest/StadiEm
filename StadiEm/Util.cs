58a01511d2a9b43703612a1e1f7afca22853fe1b
Kyle Repinski <repinski23@gmail.com>
2021-02-04 22:23:59 -0600

    Add untracked file

---
+++ StadiEm/Util.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StadiEm
+{
+	public static class Util
+	{
+		public static int ConvertRangeInt( int input, int in_min, int in_max, int out_min, int out_max )
+		{
+			return ( ( input - in_min ) * ( out_max - out_min ) / ( in_max - in_min ) ) + out_min;
+		}
+
+		public static int ConvertRangeFloat( int input, int in_min, int in_max, int out_min, int out_max )
+		{
+			return (int)( (float)(( input - in_min ) * ( out_max - out_min )) / ( in_max - in_min ) ) + out_min;
+		}
+	}
+}
